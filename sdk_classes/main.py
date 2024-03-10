ba31f0fe69d59e9a128aaa20519490a31e7e2069
AliaksandrSihai <sigai.aleksandr@mail.ru>
2024-03-10 23:17:40 +0200

    build(/sdk_classes): add sdk class

---
+++ sdk_classes/main.py
@@ -0,0 +1,9 @@
+from sdk_classes.class_sdk_weather import OpenWeatherMap
+from config.config import API_KEY
+
+if __name__ == '__main__':
+    #mode = 'polling'
+    mode = 'on_demand'
+    city = 'Vilnius'
+    weather = OpenWeatherMap(api_key=API_KEY, mode=mode)
+    print(weather.get_weather(city=city))
